a2187732ad3817a60ee1308669e682f60c3ac73c
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-02-08 17:07:01 +0530

    updates, typos correction

--- Custom Scripts/disable_MonitorMode.sh
+++ Custom Scripts/disable_MonitorMode.sh
@@ -4,6 +4,6 @@ ifconfig wlan0 down
 airmon-ng check kill
 iwconfig wlan0 mode managed
 ifconfig wlan0 up
-iwconfig
 service NetworkManager start
-echo "--->> Check if Mode:Monitor should be there."
+iwconfig
+echo "--->> Check if Mode:Managed should be there."
